cb3a3ca07b1f3738922b87cbfb6da884c3ed98f4
xairen <abhinavsrini7@gmail.com>
2024-02-07 13:03:04 -0500

    settings things up

--- tests/test_data_fetcher.py
+++ tests/test_data_fetcher.py
@@ -1,8 +1,12 @@
 from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
+import os
+from dotenv_vault import load_dotenv
+
+load_dotenv()
 
 # Replace 'YOUR_API_KEY' with your actual Alpha Vantage API key
-api_key = '5T6SATLY9GEM1CE7'
+api_key = os.getenv(API_KEY)
 
 # Initialize the TimeSeries class with your API key
 ts = TimeSeries(key=api_key, output_format='pandas')
